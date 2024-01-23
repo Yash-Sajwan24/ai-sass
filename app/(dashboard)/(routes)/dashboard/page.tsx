dc7ceaa914392cc0c95db2dee7869e292964ab30
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2024-01-24 04:59:24 +0530

    auth added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function DasboardPage() {
   return (
     <>
       <p>Dashboard Page (Protected)</p>
+      <div className="h-screen">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <>
     <p>This is the Landing page  </p>
-      
+     
+        <Link href="/sign-in">
+            <Button> Login</Button>
+         </Link>
+        <Link href="/sign-up">
+            <Button> Register</Button>
+         </Link>
+
     </>
   );
 }
