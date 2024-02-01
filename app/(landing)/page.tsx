91ef4c349d380f24a3a14ac0b99867f78ce844c4
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2024-02-01 21:31:52 +0530

    landing page completed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,18 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingContent } from "@/components/landing-content";
 
 export default function LandingPage() {
   return (
     <>
-    <p>This is the Landing page  </p>
-     
-        <Link href="/sign-in">
-            <Button> Login</Button>
-         </Link>
-        <Link href="/sign-up">
-            <Button> Register</Button>
-         </Link>
-
+      <div className="h-full">
+        <LandingNavbar />
+        <LandingHero />
+        <LandingContent />
+      </div>
     </>
   );
 }
